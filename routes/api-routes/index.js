c6ab6d0699205d443fcda4a614c0cf295d4b5087
Adil Hussain <adilh621@gmail.com>
2020-11-18 12:29:21 -0500

    added logout redirect route and logs user out

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -11,5 +11,9 @@ router.post("/api/login", passport.authenticate("local"), (req, res) => {
       id: req.user.id
     });
   });
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
 module.exports = router;
