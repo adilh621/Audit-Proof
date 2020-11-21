336365b45fbb2cddf79cdca370cdcc71296556bb
Sitora Alexander <sitoraalexander@gmail.com>
2020-11-21 10:57:20 -0500

    updated index.js

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const commentsRoute = require("./comments");
 const router = express.Router();
+router.get('/',
+    (req, res) => {
+        let user = true
+        let page = 'index'
+        let params = {
+            user: user,
+            title: 'Home',
+            style: page,
+            js: page
+        }
+        res.render(page, params)
+    }
+);
 
 router.use("/comments", commentsRoute);
 
